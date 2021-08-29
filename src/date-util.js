9719b16591bec2901f1e52a4db167c7a1252a77c
Diorgenes Morais <diorgenesmorais@gmail.com>
2021-08-28 23:59:07 -0300

    feat: calcular quantos dias incrementar

--- src/date-util.js
+++ src/date-util.js
@@ -11,6 +11,27 @@
     return dayOfWeek == 0 ? days : (days - 1)
 }
 
+/**
+ * Calcular quantos dias a incrementar
+ * @param {number} dayOfWeek (0 = domingo)
+ * @returns quantos dias a incrementar
+ */
+ const nextBusinessDay = dayOfWeek => {
+    switch(dayOfWeek) {
+        case 0:
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+            return 1
+        case 5:
+            return 3
+        case 6:
+            return 2
+    }
+}
+
 export {
-    calculateDayToAdd
+    calculateDayToAdd,
+    nextBusinessDay
 }
--- test/date-util.test.js
+++ test/date-util.test.js
@@ -1,4 +1,4 @@
-import { calculateDayToAdd } from '../src/date-util.js'
+import { calculateDayToAdd, nextBusinessDay } from '../src/date-util.js'
 
 describe('Calcular dias para adicionar', () => {
     test('Deve acrescentar 3 dias uteis sendo hoje domingo', () => {
@@ -24,5 +24,35 @@ describe('Calcular dias para adicionar', () => {
         const sunday = 0
         const businessDay = 14
         expect(calculateDayToAdd(sunday, businessDay)).toBe(incrementDays)
+    })
+})
+
+describe('Calcular quantos dias a incrementar com base no dia da semana', () =>{
+    test('Sendo domingo, deve retornar 1 dia', () => {
+        const expected = 1
+        const sunday = 0
+        expect(nextBusinessDay(sunday)).toBe(expected)
+    })
+    test('Sendo quinta feira, deve retornar 1 dia', () => {
+        const expected = 1
+        const thursday = 4
+        expect(nextBusinessDay(thursday)).toBe(expected)
+    })
+    test('Sendo sexta feira, deve retornar 3 dias', () => {
+        const expected = 3
+        const friday = 5
+        expect(nextBusinessDay(friday)).toBe(expected)
+    })
+    test('Sendo sábado, deve retornar 2 dias', () => {
+        const expected = 2
+        const saturday = 6
+        expect(nextBusinessDay(saturday)).toBe(expected)
+    })
+    test('Sendo domingo à quinta feira, deve retornar 1 dia', () => {
+        const expected = 1
+        const daysOfWeek = [0, 1, 2, 3, 4]
+        daysOfWeek.forEach(day => {
+            expect(nextBusinessDay(day)).toBe(expected)
+        })
     })
 })
