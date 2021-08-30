23dc5fa87bc6c92bc4ca1313853ea009aaae6f42
Diorgenes Morais <diorgenesmorais@gmail.com>
2021-08-30 19:25:40 -0300

    feat: como alterar o fluxo

--- src/asynchronous.flow.with.observer.js
+++ src/asynchronous.flow.with.observer.js
@@ -1,7 +1,25 @@
-const flow = async () => {
+const flow = async value => {
     return new Promise((resolve, reject) => {
-        
+        if (value) {
+            reject({ message: 'rejeitado' })
+        }
+        resolve({ message: 'resolvido'})
     })
 }
 
-export default flow
+const submit = async condition => {
+    try {
+        await flow(condition)
+    } catch (error) {
+        return error
+    }
+
+    return {
+        message: 'seguiu o fluxo'
+    }
+}
+
+export {
+    flow,
+    submit
+}
--- test/asynchronous.flow.with.observer.test.js
+++ test/asynchronous.flow.with.observer.test.js
@@ -1,4 +1,4 @@
-import flow from '../src/asynchronous.flow.with.observer.js'
+import { flow, submit } from '../src/asynchronous.flow.with.observer.js'
 
 describe('Alterar fluxo conforme resultado da promise', () => {
     test('Deve ser uma função', () => {
@@ -7,5 +7,25 @@ describe('Alterar fluxo conforme resultado da promise', () => {
 
     test('Deve retornar uma promise', async () => {
         expect(flow()).toBeInstanceOf(Promise)
+    })
+
+    test('Deve impedir de executar o erro', async () =>{
+        try {
+            await flow(true)
+            // aqui seria um ponto crítico
+            throw new Error('Não alterou o fluxo!')
+        } catch ({ message }) {
+            expect(message).toBe('rejeitado')
+        }
+    })
+
+    test('Deve alterar o fluxo', async () => {
+        const { message } = await submit(true)
+        expect(message).toBe('rejeitado')
+    })
+
+    test('Deve seguir o fluxo', async () => {
+        const { message } = await submit(false)
+        expect(message).toBe('seguiu o fluxo')
     })
 })
